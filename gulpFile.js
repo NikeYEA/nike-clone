f985d0288c01f4f67e27d7e03e06c37659f1e47e
David Danielson <David@Davids-MacBook.local>
2016-11-28 12:17:46 -0700

    gulped all js and css files. gulp is now running nodemon as well

--- gulpFile.js
+++ gulpFile.js
@@ -1,37 +1,37 @@
-// var gulp = require('gulp');
-// var concat = require("gulp-concat");
-// var annotate = require("gulp-ng-annotate");
-// var css = require("gulp-css");
-// var nodemon = require('gulp-nodemon');
-//
-// var paths = {
-//     jsSource: ['public/app/**/*.js'],
-//     cssSource: ['public/app/**/*.css'],
-//     server: ['server/index.js']
-// };
-//
-// gulp.task('serve', function() {
-//     nodemon({
-//         'script': paths.server
-//     });
-// });
-//
-// gulp.task('css', function() {
-//     gulp.src(paths.cssSource)
-//         .pipe(concat('bundle.css'))
-//         .pipe(gulp.dest('./public/dist'));
-// });
-//
-// gulp.task('js', function() {
-//     gulp.src(paths.jsSource)
-//         .pipe(annotate())
-//         .pipe(concat('bundle.js'))
-//         .pipe(gulp.dest('./public/dist'));
-// });
-//
-// gulp.task('watch', function() {
-//     gulp.watch(paths.jsSource, ['js']);
-//     gulp.watch(paths.cssSource, ['css']);
-// });
-//
-// gulp.task('default', ['js', 'css', 'watch', 'serve']);
+var gulp = require('gulp');
+var concat = require("gulp-concat");
+var annotate = require("gulp-ng-annotate");
+var css = require("gulp-css");
+var nodemon = require('gulp-nodemon');
+
+var paths = {
+    jsSource: ['public/app/**/*.js'],
+    cssSource: ['public/app/**/*.css'],
+    server: ['server/index.js']
+};
+
+gulp.task('serve', function() {
+    nodemon({
+        'script': paths.server
+    });
+});
+
+gulp.task('css', function() {
+    gulp.src(paths.cssSource)
+        .pipe(concat('bundle.css'))
+        .pipe(gulp.dest('./public/dist'));
+});
+
+gulp.task('js', function() {
+    gulp.src(paths.jsSource)
+        .pipe(annotate())
+        .pipe(concat('bundle.js'))
+        .pipe(gulp.dest('./public/dist'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch(paths.jsSource, ['js']);
+    gulp.watch(paths.cssSource, ['css']);
+});
+
+gulp.task('default', ['js', 'css', 'watch', 'serve']);
---
+++ public/dist/bundle.js
@@ -0,0 +1,68 @@
+angular.module('nike-clone', ['ui.router']).config(["$stateProvider", "$urlRouterProvider", function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/home');
+
+  $stateProvider
+    .state('home', {
+      templateUrl: './app/routes/home/home.html',
+      controller: 'homeCtrl',
+      url: '/home'
+    })
+    .state('men', {
+      templateUrl: './app/routes/men/men.html',
+      controller: 'menCtrl',
+      url: '/men'
+    })
+    .state('women', {
+      templateUrl: './app/routes/women/women.html',
+      controller: 'womenCtrl',
+      url: '/women'
+    })
+    .state('boys', {
+      templateUrl: './app/routes/boys/boys.html',
+      controller: 'boysCtrl',
+      url: '/boys'
+    })
+    .state('girls', {
+      templateUrl: './app/routes/girls/girls.html',
+      controller: 'girlsCtrl',
+      url: '/girls'
+    })
+}])
+
+angular.module('nike-clone').service('mainService', ["$http", function($http) {
+  
+}])
+
+angular.module('nike-clone').directive('footer', function() {
+  return {
+    restrict: 'AE',
+    templateUrl: './footer.html'
+  }
+})
+
+angular.module('nike-clone').directive('header', function() {
+  return {
+    restrict: 'AE',
+    templateUrl: './header.html'
+  }
+})
+
+angular.module('nike-clone').controller('boysCtrl', ["$scope", function($scope) {
+
+}])
+
+angular.module('nike-clone').controller('homeCtrl', ["$scope", function($scope) {
+
+}])
+
+angular.module('nike-clone').controller('girlsCtrl', ["$scope", function($scope) {
+
+}])
+
+angular.module('nike-clone').controller('menCtrl', ["$scope", function($scope) {
+  
+}])
+
+angular.module('nike-clone').controller('womenCtrl', ["$scope", function($scope) {
+
+}])
