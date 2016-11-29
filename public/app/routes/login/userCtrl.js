9b8561d8e3ffcb228309a575d3087a0c3887a327
akshar <aksharp99@gmail.com>
2016-11-29 11:36:50 -0700

    user populated

--- public/app/routes/login/userCtrl.js
+++ public/app/routes/login/userCtrl.js
@@ -1,4 +1,4 @@
-angular.module("app")
+angular.module("nike-clone")
 	.controller("userCtrl", function($scope, mainService) {
 
 		$scope.getUsers = function() {
--- server/db/user_create.sql
+++ server/db/user_create.sql
@@ -1,3 +1,3 @@
-INSERT INTO users (first_name, last_name, email, address, city, zip, country)
-VALUES ($1, $2, $3, $4, $5, $6, $7)
+INSERT INTO users (first_name, email)
+VALUES ($1, $2)
 RETURNING *;
