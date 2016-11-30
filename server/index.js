eba86ccbe28cf50c03b6a013f7eccfe119527e27
Josh Bangerter <joshbang@me.com>
2016-11-30 10:46:44 -0700

    implemented twilio

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ var cors = require('cors');
 var massive = require('massive');
 var bodyParser = require('body-parser');
 var config = require('../config');
+var client = require('twilio')(config.accountSid, config.authToken);
 var app = module.exports = express();
 app.use(bodyParser.json());
 app.use(cors());
@@ -36,6 +37,39 @@ app.post('/api/add/item/cart/:orderid', productCtrl.addToCart);
 app.put('/api/update/qty/:productid', productCtrl.updateProductInCart);
 app.delete('/api/delete/item/cart/:productid', productCtrl.deleteCartItem);
 
+// TWILIO //
+app.get('/testtwilio', function(req, res) {
+	client.sendMessage({
+		to:'+18016945874',
+		from: '+13857071154',
+		body: 'Yo what up, booooiiiissss'
+	}, function(err, data) {
+		if(err) {
+			console.log(err);
+		} else {
+			console.log(data);
+		}
+	})
+})
+// client.sendMessage({
+//
+//     to:'+18016945874', // Any number Twilio can deliver to
+//     from: '+13857071154', // A number you bought from Twilio and can use for outbound communication
+//     body: 'word to your mother.' // body of the SMS message
+//
+// }, function(err, responseData) { //this function is executed when a response is received from Twilio
+//
+//     if (!err) { // "err" is an error received during the request, if any
+//
+//         // "responseData" is a JavaScript object containing data received from Twilio.
+//         // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
+//         // http://www.twilio.com/docs/api/rest/sending-sms#example-1
+//
+//         console.log(responseData.from); // outputs "+14506667788"
+//         console.log(responseData.body); // outputs "word to your mother."
+//
+//     }
+// });
 
 app.listen(port, function() {
 	console.log('Listening on port ' + port);
