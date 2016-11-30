dd4ef06ad874eaf91ae5b3c4fce8a1dd1ea54d1c
Josh Bangerter <joshbang@me.com>
2016-11-30 10:48:06 -0700

    made code cleaner

--- server/index.js
+++ server/index.js
@@ -51,25 +51,7 @@ app.get('/testtwilio', function(req, res) {
 		}
 	})
 })
-// client.sendMessage({
-//
-//     to:'+18016945874', // Any number Twilio can deliver to
-//     from: '+13857071154', // A number you bought from Twilio and can use for outbound communication
-//     body: 'word to your mother.' // body of the SMS message
-//
-// }, function(err, responseData) { //this function is executed when a response is received from Twilio
-//
-//     if (!err) { // "err" is an error received during the request, if any
-//
-//         // "responseData" is a JavaScript object containing data received from Twilio.
-//         // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
-//         // http://www.twilio.com/docs/api/rest/sending-sms#example-1
-//
-//         console.log(responseData.from); // outputs "+14506667788"
-//         console.log(responseData.body); // outputs "word to your mother."
-//
-//     }
-// });
+
 
 app.listen(port, function() {
 	console.log('Listening on port ' + port);
