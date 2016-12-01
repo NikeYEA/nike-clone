b20ea79864d03da78ee227d852be448edbeac764
akshar <aksharp99@gmail.com>
2016-12-01 14:43:24 -0700

    integration

--- public/dist/bundle.js
+++ public/dist/bundle.js
@@ -32,16 +32,29 @@ angular.module('nike-clone', ['ui.router']).config(["$stateProvider", "$urlRoute
       controller: 'customizeCtrl',
       url: '/customize'
     })
-    .state('login', {
-      templateUrl: './app/routes/login/login.html',
-      controller: 'userCtrl',
-      url: '/user'
-    })
-    .state('stuff', {
-      templateUrl: './app/routes/stuff/stuff.html',
-      controller: 'stuffCtrl',
-      url: '/stuff'
-    })
+  .state('login', {
+    url: '/login',
+    templateUrl: './app/routes/login/login.html',
+    controller: 'loginCtrl'
+  })
+  .state('profile', {
+    url: '/profile',
+    templateUrl: './app/routes/profile/profile.html',
+    controller: 'profileCtrl',
+    resolve: {
+      user: ["authService", "$state", function(authService, $state) {
+        return authService.getCurrentUser()
+          .then(function(response) {
+            if (!response.data.email) {
+              return $state.go('login');
+            }
+            return response.data
+          }).catch(function(err) {
+            $state.go('login');
+          });
+      }]
+    }
+  });
 
 
 
@@ -136,15 +149,61 @@ angular.module('nike-clone').service('mainService', ["$http", function($http) {
 
 }]);
 
-angular.module('nike-clone').directive('headerDir', function() {
-  return {
-    restrict: 'E',
-    templateUrl: './app/directives/header/header.html',
-    controller: ["$scope", function($scope) {
-      
-    }]
-  }
-})
+// INITILIZE SERVICE
+// ============================================================
+angular.module("app").service("authService", ["$http", function($http) {
+
+  // AUTH FUNCTIONS
+  // ============================================================
+  this.login = function(user) {
+    return $http({
+      method: 'post',
+      url: '/api/login',
+      data: user
+    }).then(function(response) {
+      return response;
+    });
+  };
+  this.logout = function() {
+    return $http({
+      method: 'get',
+      url: '/api/logout'
+    }).then(function(response) {
+      return response;
+    });
+  };
+  this.getCurrentUser = function() {
+    return $http({
+      method: 'GET',
+      url: '/api/me'
+    }).then(function(response) {
+      return response;
+    });
+  };
+  this.register = function(user) {
+    return $http({
+      method: 'POST',
+      url: '/api/register',
+      data: user
+    }).then(function(response) {
+      return response;
+    });
+  };
+  // this.editUser = function(id, user) {
+  //   return $http({
+  //     method: 'PUT',
+  //     url: "/api/user/current",
+  //     data: user
+  //   }).then(function(response) {
+  //     return response;
+  //   });
+  // };
+
+  // OTHER FUNCTIONS
+  // ============================================================
+
+
+}]);
 
 angular.module('nike-clone').directive('footerDir', function() {
   return {
@@ -152,6 +211,16 @@ angular.module('nike-clone').directive('footerDir', function() {
     templateUrl: './app/directives/footer/footer.html',
     controller: ["$scope", function($scope) {
 
+    }]
+  }
+})
+
+angular.module('nike-clone').directive('headerDir', function() {
+  return {
+    restrict: 'E',
+    templateUrl: './app/directives/header/header.html',
+    controller: ["$scope", function($scope) {
+      
     }]
   }
 })
@@ -172,31 +241,81 @@ angular.module('nike-clone').controller('homeCtrl', ["$scope", function($scope) 
 
 }])
 
+// INITILIZE CONTROLLER
+// ============================================================
+angular.module("app").controller("loginCtrl", ["$scope", "authService", "$state", "mainService", function($scope, authService, $state, mainService) {
+
+  // VARIABLES
+  // ============================================================
+  $scope.test= "login";
+
+  // FUNCTIONS
+  // ============================================================
+  $scope.login = function(user) {
+    authService.login(user)
+    .then(function(response) {
+      if (!response.data) {
+        $scope.user.password = "";
+        return alert('user could not be logged in');
+      }
+      $state.go('profile')
+    }).catch(function(err) {
+      $scope.user.password = "";
+      alert('user could not be logged in');
+    });
+  };
+
+  $scope.register = function(newUser) {
+    authService.register(newUser)
+    .then(function(response) {
+      if (response.data !== "User created successfully!") {
+        return alert("Could not register user");
+      }
+      alert(response.data);
+    }).catch(function(err) {
+      return alert("Could not register user");
+    });
+  };
+
+  $scope.getUsers = function() {
+    mainService.getUsers()
+      .then(function(response) {
+        $scope.users = response.data;
+        console.log(response);
+
+      });
+  };
+
+  $scope.postUsers = function(fname,email){
+    mainService.postUsers(fname,email).then(function(response){
+      console.log('this is the postuser response: ',response);
+    })
+  }
+  $scope.getUsers();
+
+}]);
+
 angular.module('nike-clone').controller('menCtrl', ["$scope", function($scope) {
   
 }])
 
-angular.module("nike-clone")
-	.controller("userCtrl", ["$scope", "mainService", function($scope, mainService) {
+// INITILIZE CONTROLLER
+// ============================================================
+angular.module("app").controller("profileCtrl", ["$scope", "authService", "user", "$state", function($scope, authService, user, $state) {
 
-		$scope.getUsers = function() {
-			mainService.getUsers()
-				.then(function(response) {
-					$scope.users = response.data;
-					console.log(response);
+  // VARIABLES
+  // ============================================================
+  $scope.user = user
 
-				});
-		};
+  // FUNCTIONS
+  // ============================================================
+  $scope.logout = function() {
+    authService.logout().then(function(response) {
+      $state.go('login');
+    });
+  };
 
-		$scope.postUsers = function(fname,email){
-			mainService.postUsers(fname,email).then(function(response){
-				console.log('this is the postuser response: ',response);
-			})
-		}
-		$scope.getUsers();
-
-		
-	}]);
+}]);
 
 angular.module('nike-clone').controller('stuffCtrl', ["$scope", function($scope) {
   $scope.test = "test";
