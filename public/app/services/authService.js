dfb7bb6c352390b45baeb21bf7a767843fa30ef2
Josh Bangerter <joshbang@me.com>
2016-12-01 14:18:21 -0700

    service

---
+++ public/app/services/authService.js
@@ -0,0 +1,55 @@
+// INITILIZE SERVICE
+// ============================================================
+angular.module("app").service("authService", function($http) {
+
+  // AUTH FUNCTIONS
+  // ============================================================
+  this.login = function(user) {
+    return $http({
+      method: 'post',
+      url: '/api/login',
+      data: user
+    }).then(function(response) {
+      return response;
+    });
+  };
+  this.logout = function() {
+    return $http({
+      method: 'get',
+      url: '/api/logout'
+    }).then(function(response) {
+      return response;
+    });
+  };
+  this.getCurrentUser = function() {
+    return $http({
+      method: 'GET',
+      url: '/api/me'
+    }).then(function(response) {
+      return response;
+    });
+  };
+  this.register = function(user) {
+    return $http({
+      method: 'POST',
+      url: '/api/register',
+      data: user
+    }).then(function(response) {
+      return response;
+    });
+  };
+  // this.editUser = function(id, user) {
+  //   return $http({
+  //     method: 'PUT',
+  //     url: "/api/user/current",
+  //     data: user
+  //   }).then(function(response) {
+  //     return response;
+  //   });
+  // };
+
+  // OTHER FUNCTIONS
+  // ============================================================
+
+
+});
